61c01fea4c88e974767ccd37e8657f1d421553f6
Shrikrishna Ubhale <111643792+ssubhale@users.noreply.github.com>
2025-02-26 15:51:55 +0530

    Update 144_statement_reading_2.c

--- 144_statement_reading_2.c
+++ 144_statement_reading_2.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
-int main(void)
-{
+// 24) void ( * p )( int, void ( * )( int ) ) )( int );
+
+void Fun1(int);
+
+void Fun2(int);
+
+void (*Func(int, void (*)(int)))(int);
+
+
+int main(void) {
+
+    void (*(*pPtr)(int, void (*)(int)))(int);
+
+    pPtr = Func;
+
+    void (*pResult)(int) = pPtr(5, Fun1);
+
+    pResult(10);
 
     return 0;
+}
+
+
+void Fun1(int iNo)
+{
+    printf("Fun1 function called : %d\n", iNo);
+}
+
+void Fun2(int iNo)
+{
+    printf("Fun2 function called : %d\n", iNo);
+}
+
+void (*Func(int iNo, void (*Fun1)(int)))(int)
+{
+    printf("Func function called : %d\n", iNo);
+    Fun1(iNo);
+    return Fun2;
 }
 
